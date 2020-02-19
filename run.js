a26731377724e7db195c59c73d888fd98b6b77b4
Annie J <damaged_people@telia.com>
2020-02-19 21:55:27 +0100

    Added some mobile styling

--- run.js
+++ run.js
@@ -8,9 +8,11 @@ APP.Run = function() {
 APP.GetFullPoem = function() {
     APP.GetPoems()
     .then(result => {
-        let poems = result.values;
-        let poem = poems[Math.floor(Math.random() * poems.length)];
-        APP.AddFragments(poem);
+        if (result) {
+            let poems = result.values;
+            let poem = poems[Math.floor(Math.random() * poems.length)];
+            APP.AddFragments(poem);
+        }
     })
 }
 
@@ -18,33 +20,35 @@ APP.GetFullPoem = function() {
 APP.GeneratePoem = function () {
     APP.GetPoems()
     .then(result => { 
-        let poems = result.values;
-        let selectedPoems = [];
-        let selectedFragments = [];
+        if (result) {
+            let poems = result.values;
+            let selectedPoems = [];
+            let selectedFragments = [];
 
-        // Shuffle and select first 2-4 poems (TODO: make these numbers into user inputs)
-        poems.sort(function() { return 0.5 - Math.random() });
-        selectedPoems = poems.slice(0, Math.floor(Math.random() * 3) + 2);
+            // Shuffle and select first 2-4 poems (TODO: make these numbers into user inputs)
+            poems.sort(function() { return 0.5 - Math.random() });
+            selectedPoems = poems.slice(0, Math.floor(Math.random() * 3) + 2);
 
-        // For each selecte poem, select fragment
-        selectedPoems.forEach((poem, i) => {
-            let fragment;
+            // For each selecte poem, select fragment
+            selectedPoems.forEach((poem, i) => {
+                let fragment;
 
-            // If first, select first fragment
-            if (i === 0) {
-                fragment = poem[0];
-            } // If last, select last fragment
-            else if (i === selectedPoems.length - 1) {
-                fragment = poem[poem.length - 1];
-            }
-            else { // Else, randomize
-                fragment = poem[Math.floor(Math.random() * poem.length)];
-            }
+                // If first, select first fragment
+                if (i === 0) {
+                    fragment = poem[0];
+                } // If last, select last fragment
+                else if (i === selectedPoems.length - 1) {
+                    fragment = poem[poem.length - 1];
+                }
+                else { // Else, randomize
+                    fragment = poem[Math.floor(Math.random() * poem.length)];
+                }
 
-            selectedFragments.push(fragment);
-        });
+                selectedFragments.push(fragment);
+            });
 
-        APP.AddFragments(selectedFragments);
+            APP.AddFragments(selectedFragments);
+        }
     });
 }
 
@@ -74,7 +78,7 @@ APP.AddFragments = function(fragments) {
 
 /// Gets poems from Google Spreadsheet.
 APP.GetPoems = function() {
-    return fetch('https://sheets.googleapis.com/v4/spreadsheets/1QbTVndIZn95DymZKldJjtOGkEJn3ab6KQdkUSX1GWZY/values/A1:D100?key=AIzaSyAROUHOYJS_06C6J6b2rjoxJszMTXgeRho')
+    return fetch('https://sheets.googleapis.com/v4/spreadsheets/1QbTVndIZn95DymZKldJjtOGkEJn3ab6KQdkUSX1GWZY/values/A1:D100?key=AIzaSyCkFFeHiQNkm_KNzV3WP7uNxWFZlzpAPQ4')
     .then(data => { 
         if(data.ok) {
             return data.json();
